1885d2ee894a0261d106963f85bd3d61fb0a4889
jm005807 <bing.zhang@baozun.com>
2019-03-13 16:16:32 +0800

    新增库存中心服务

---
+++ shop-inventory/src/main/java/com/zb/onlineshop/shopinventory/ShopInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.zb.onlineshop.shopinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopInventoryApplication.class, args);
+    }
+
+}
